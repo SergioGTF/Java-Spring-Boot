bb54e7c0910acfa562ab051ae782734d17112af2
Sergiogtf <sergio23300033@aluno.cesupa.br>
2024-08-14 22:37:14 -0300

    Java Spring Boot :rocket:

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Java-Spring-Boot
+# Java-Spring-Boot :rocket:
---
+++ src/main/java/br/com/sergiogtf/todolist/Users/UserModel.java
@@ -0,0 +1,7 @@
+package br.com.sergiogtf.todolist.Users;
+
+public class UserModel {
+    public String username;
+    public String name;
+    public String password;
+}
